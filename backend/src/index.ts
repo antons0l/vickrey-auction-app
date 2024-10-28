3f2a86d10bb009d355718ef8de7f08f4d2b0ab55
Anton Solovyov <antonsolovyov@gmail.com>
2024-10-27 20:03:46 -0400

    fix CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,19 +6,26 @@ export type Env = {
   DB: D1Database
 }
 
-const app = new Hono<{ Bindings: Env }>()
+const app = new Hono<{ Bindings: Env }>();
+
+app.use('*', async (c, next) => {
+  await next();
+  c.header('Access-Control-Allow-Origin', 'https://vickrey-auction-app.pages.dev');
+  c.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  c.header('Access-Control-Allow-Headers', 'Content-Type');
+});
 
 app.get('/api/auctions', async (c) => {
   const db = drizzle(c.env.DB);
   const result = await db.select().from(auctions).all();
-  return c.json(result);
+  return c.json(result, 200);
 });
 
 app.post('/api/auctions', async (c) => {
   const db = drizzle(c.env.DB);
   const { title } = await c.req.json();
   const result = await db.insert(auctions).values({title}).returning();
-  return c.json(result);
+  return c.json(result, 200);
 })
 
 export default app
--- frontend/src/utils/urlBuilder.ts
+++ frontend/src/utils/urlBuilder.ts
@@ -1,3 +1,3 @@
 export const urlBuilder = (path: string): string => {
-  return (import.meta.env.VITE_NODE_ENV === "development") ? path : `${import.meta.env.VITE_API_URL}/api/`;
+  return (import.meta.env.VITE_NODE_ENV === "development") ? path : `${import.meta.env.VITE_API_URL}${path}`;
 }
